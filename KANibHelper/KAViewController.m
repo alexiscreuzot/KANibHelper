6e58ac00fda3a07167a3359196ce84f58b5cd296
Alexis Creuzot <alexis@mobizel.com>
2013-04-16 16:31:18 +0200

    Changed name conventions to closer from apple ones

--- KANibHelper/KAViewController.m
+++ KANibHelper/KAViewController.m
@@ -16,17 +16,21 @@
     return [self initWithNibName:nibName bundle:nil];
 }
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)initWithNibName:(NSString *)nibName bundle:(NSBundle *)nibBundleOrNil
 {
-    if(nibNameOrNil){
-        NSString *nibName4inches =  [nibNameOrNil stringByAppendingString:@"_4in"];
+    if(nibName){
+        NSString *nibName4inches =  [nibName stringByAppendingString:@"~iphone4"];
+        NSString *nibName3_5inches =  [nibName stringByAppendingString:@"~iphone3_5"];
         NSString *path4inches = [[NSBundle mainBundle] pathForResource:nibName4inches ofType:@"nib"];
+        NSString *path3_5inches = [[NSBundle mainBundle] pathForResource:nibName3_5inches ofType:@"nib"];
         
         CGRect screenBounds = [[UIScreen mainScreen] bounds];
         if(screenBounds.size.height == 568 && path4inches){
             self = [super initWithNibName:nibName4inches bundle:nibBundleOrNil];
+        }else if(screenBounds.size.height == 480 && path3_5inches){
+            self = [super initWithNibName:nibName3_5inches bundle:nibBundleOrNil];
         }else{
-            self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+            self = [super initWithNibName:nibName bundle:nibBundleOrNil];
         }
     }else{
         self = [self init];
